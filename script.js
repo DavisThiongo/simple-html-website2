ba62c1e2de050b9e6a22edb6bffc225a25c1087b
Davis Kamau Thiong'o <davisthiongo27@gmail.com>
2026-01-06 10:42:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const toggleBtn = document.getElementById("themeToggle");
 toggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 });
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thank you! Your message has been sent (demo).");
+    form.reset();
+});

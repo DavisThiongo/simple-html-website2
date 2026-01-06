142f14ad4458f70fcf35dd1c655da62baf2428a2
Davis Kamau Thiong'o <davisthiongo27@gmail.com>
2026-01-06 14:57:55 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,28 @@ const toggleBtn = document.getElementById("themeToggle");
 toggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 });
+
 const form = document.getElementById("contactForm");
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
     alert("Thank you! Your message has been sent (demo).");
     form.reset();
+});
+// Scroll Fade-In
+const faders = document.querySelectorAll('.scroll-fade');
+
+const appearOnScroll = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target); // only animate once
+        }
+    });
+}, {
+    threshold: 0.1
+});
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
 });
